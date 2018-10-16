63828b53fbad2e8beaf37c13fd5084021c41301b
Marta <Marta@172.16.6.107>
2018-10-16 17:12:10 +0200

    Refinado el código de paso de una pregunta a otra y establecidos los primeros pasos de respuestas correctas y sistema de vidas.

--- juego.php
+++ juego.php
@@ -30,12 +30,25 @@ $preguntaActual = rand(0, $numPreguntas - 1);
 
 <div>
     <p><a id="enunciado" class=""></a></p>
-    <p><a id="r1" class="btn btn-block btn-primary"></a></p>
-    <p><a id="r2" class="btn btn-block btn-primary"></a></p>
-    <p><a id="r3" class="btn btn-block btn-primary"></a></p>
-    <p><a id="r4" class="btn btn-block btn-primary"></a></p>
-    <p><a class="btn btn-block btn-warning" onclick="volver();">Volver al Menú</a></p>
+    <p><a id="respuesta" class="" style="display:none;"></a></p> <!--Vamos a guardar en una caja invisible el valor de la respuesta correcta-->
+    <p><a id="r1" class="botonRespuesta btn btn-block btn-primary" value="1"></a></p> <!--Value le va a dar un valor que vamos a utilizar para saber cual es la respuesta correcta-->
+    <p><a id="r2" class="botonRespuesta btn btn-block btn-primary" value="2"></a></p>
+    <p><a id="r3" class="botonRespuesta btn btn-block btn-primary" value="3"></a></p>
+    <p><a id="r4" class="botonRespuesta btn btn-block btn-primary" value="4"></a></p>
 
+    
+    
+</div>
+<div class="row">
+  <div class="col-3">
+      <p><a class="btn btn-primary disabled">Vidas <br/><span id="lives">3</span>/3</a></p>
+  </div>
+  <div class="col-6">
+      <p><a id="respuestas" class="btn btn-primary disabled">Respuestas Correctas <br/> <span id="correctas">0</span>/10</a></p>   
+  </div>
+  <div class="col-3">
+      <p><a class="btn btn-warning" onclick="volver();">Volver <br/> al Menú</a></p>
+  </div>
 </div>
 
 <script>
@@ -45,32 +58,67 @@ $preguntaActual = rand(0, $numPreguntas - 1);
 
     var listaPreguntas = <?php echo json_encode($listaPreguntas); ?>;
     var numeroPregunta = Math.floor(Math.random() * listaPreguntas.length);
+    var arrayAuxiliar=[numeroPregunta];
+    
+    var correctas=<?php echo $correctas?>; //tomamos los valores del PHP.
+    var vidas=<?php echo $vidas?>;
+    
     var contador = 0;
     sigue();
     // console.log(listaPreguntas[numeroPregunta]);
 
-    function sigue() {
-        if (contador < 10) {
+   //La función escribe los datos sobre los "labels".
+    function rellenaDatos(){
+         if (contador < 10 || vidas>0) {
             numeroPregunta = Math.floor(Math.random() * listaPreguntas.length);
             $('#enunciado').text(listaPreguntas[numeroPregunta][1]);
-            $('#r1').text(listaPreguntas[numeroPregunta][2]).click(function () {
-                sigue();
-                stopImmediatePropagation();
-            });
-            $('#r2').text(listaPreguntas[numeroPregunta][3]).click(function () {
-                sigue();
-                stopImmediatePropagation();
-            });
-            $('#r3').text(listaPreguntas[numeroPregunta][4]).click(function () {
-                sigue();
-                stopImmediatePropagation();
-            });
-            $('#r4').text(listaPreguntas[numeroPregunta][5]).click(function () {
-                sigue();
-                stopImmediatePropagation();
-            });
+            $('#respuesta').text(listaPreguntas[numeroPregunta][6]);
+            $('#r1').text(listaPreguntas[numeroPregunta][2])
+             
+            $('#r2').text(listaPreguntas[numeroPregunta][3])
+             
+            $('#r3').text(listaPreguntas[numeroPregunta][4])
+            
+            $('#r4').text(listaPreguntas[numeroPregunta][5])
+            
             contador++;
+            arrayAuxiliar.push(numeroPregunta);
+            //console.log(arrayAuxiliar);         
         }
     }
+    
+    //La función ejecuta rellenaDatos, estableciendo un único click para que no sea redundante.
+    
+    function sigue() {
+       
+        rellenaDatos();
+        //Cuando se hace click sobre cualquiera de los botones;
+        $( ".botonRespuesta" ).click(function() {
+           console.log($(this).attr("value"));
+           
+           var valorDelBoton = $(this).attr("value");  //This es el último evento de click. Lee el valor del atributo "value".
+           var valorDeRespuesta = $('#respuesta').text(); //Leemos el párrafo respuesta y obtenemos su valor.
+           if(contador<10 || vidas>0){
+             if(valorDelBoton == valorDeRespuesta){ //En caso de que ambos valores sean iguales, suma uno a correctas y actualiza el texto.
+               correctas=correctas+1;
+                $('#correctas').text( correctas);
+               rellenaDatos(); //Se ejecuta de nuevo la función "rellenaDatos" de manera limpia y efectiva.
+             }else{
+                vidas=vidas-1;
+                $('#lives').text(vidas);
+                rellenaDatos();
+             }
+       }
+           
+           console.log("respuestas correctas" + correctas);
+        console.log("Las vidas son;" + vidas);
+           
+           
+        });
+
+    }
+    
+  
+    
 
 </script>
