631c7d4f07e906be7714fecf0b2895081d916d22
Alejandro <alejandro@xellex.es>
2018-10-24 16:47:55 +0200

    Añadida fuente mvBoli.

--- index.php
+++ index.php
@@ -12,23 +12,23 @@ if (session_status() == PHP_SESSION_NONE) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <title>Trivi-All</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     </head>
     <body style="background-color: #E4EDFF">
         <div class="container" id="principal" style="background-color: #5591FF">
-            <div class="row" style="border:1px solid black; background-color: blue; color: white; height: 15vw">
+            <div class="row" style=" background-color: blue; color: white; height: 15vw">
                 <div class="col-12 text-center">
                     <h2 class="text-center">Encabezado y logo</h2>
                 </div>
             </div>
-            <div class="row" style="border:1px solid black;">
-                <div class="col-8" style="border:1px solid black;"></div>
-                <div class="col-2" style="border:1px solid black;">
+            <div class="row" style="">
+                <div class="col-8"></div>
+                <div class="col-2">
                     <br/>
                     <button id="buttonRegistro" class="btn btn-primary btn-block" type="submit">Registro</button>
                     <br/>
                 </div>
-                <div class="col-2" style="border:1px solid black;">
+                <div class="col-2" style="">
                     <br/>
                     <button id="buttonLogin" class="btn btn-primary btn-block" type="submit">Login</button>
                     <button id="buttonProfile" class="btn btn-primary btn-block" type="submit"><?php echo $_SESSION['nombreUsuario'] ?></button>
@@ -37,8 +37,8 @@ if (session_status() == PHP_SESSION_NONE) {
                 </div>
             </div>
             <div class="row">
-                <div class="col-3" style="border:1px solid black;"></div>
-                <div class="col-md-6" style="border:1px solid black; color: #000;" id="menu">
+                <div class="col-3"></div>
+                <div class="col-md-6" style="color: #000; font-family: mvBoli;" id="menu">
                     <br/>
                     <!-- 
                        Colores:
@@ -55,9 +55,9 @@ if (session_status() == PHP_SESSION_NONE) {
                     <p><a id="buttonLengua" class="btn btn-block" style="background-color: #FFFF43" onclick="sigue('4')">Lengua</a></p>
                     <p><a id="buttonHistoria" class="btn btn-block" style="background-color: #FF9843" onclick="sigue('5')">Historia</a></p>
                 </div>
-                <div class="col-3" style="border:1px solid black;"></div>
+                <div class="col-3" style=""></div>
             </div>
-            <div class="row" style="border:1px solid black; height: 70px;">
+            <div class="row" style=" height: 70px;">
                 <div class="col-12"></div>
             </div>
         </div>
